31909dce252b001e5c0245cb8f06715102775233
AlexPh7 <>
2022-12-20 22:53:38 -0300

    Primeiro Commit - Jogo Craps

---
+++ src/jogocraps/JogoCraps.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package jogocraps;
+
+/**
+ *
+ * @author ph757
+ */
+public class JogoCraps {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
